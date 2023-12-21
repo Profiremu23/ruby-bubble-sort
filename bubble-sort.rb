8b32259e0a5fed023ed23b2599240ef557f6225f
Profiremu23 <profirdani23@gmail.com>
2023-12-21 19:47:17 +0100

    Added until loop statement to iterate until everything is sorted

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,17 +1,22 @@
 def bubble_sort(array)
   second_value = 0
-  array.each_with_index do |current_value, index|
-    second_value = array[index + 1]
-    if second_value.nil?
-      second_value = array[index]
-    end
+  is_sorted = false
+  until is_sorted == true do
+    array.each_with_index do |current_value, index|
+      second_value = array[index + 1]
+      if second_value.nil?
+        second_value = array[index]
+      end
 
-    if current_value > second_value
-      array.delete(current_value)
-      array.delete(second_value)
-      array.insert(index, second_value, current_value)
+      if current_value > second_value #&& is_sorted == false
+        array.delete(current_value)
+        array.delete(second_value)
+        array.insert(index, second_value, current_value)
+      else
+        is_sorted = true
+      end
+      p array
     end
-    p array
   end
 end
 
