0659c277d198a04c2f988dc21c672a96947c8456
Profiremu23 <profirdani23@gmail.com>
2023-12-22 19:45:42 +0100

    Added a "second walker" statement for bubble sorting from the back

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,21 +1,38 @@
 def bubble_sort(array)
-  second_value = 0
   is_sorted = false
   until is_sorted == true do
     array.each_with_index do |current_value, index|
-      second_value = array[index + 1]
-      if second_value.nil?
-        second_value = array[index]
+      #Getting the second value (for forward iteration)
+      next_value = array[index + 1]
+      if next_value.nil?
+        next_value = array[index]
       end
-
-      if current_value > second_value #&& is_sorted == false
+      #Sorting mechanism (forward)
+      if current_value > next_value #&& is_sorted == false
         array.delete(current_value)
-        array.delete(second_value)
-        array.insert(index, second_value, current_value)
+        array.delete(next_value)
+        array.insert(index, next_value, current_value)
       else
         is_sorted = true
       end
-      p array
+      p array #Print the result after the first iteration
+
+      #Getting the values for backward iteration (to create a two bound)
+      inverted_index = index - (index * 2) - 1
+      current_backward_value = array[inverted_index]
+      next_backward_value = array[inverted_index - 1]
+      if next_backward_value.nil?
+        next_backward_value = array[inverted_index]
+      end
+      #Sorting mechanism (backward)
+      if current_backward_value < next_backward_value #&& is_sorted == false
+        array.delete(current_backward_value)
+        array.delete(next_backward_value)
+        array.insert(inverted_index, current_backward_value, next_backward_value)
+      else
+        is_sorted = true
+      end
+      p array #Print the result after the second iteration
     end
   end
 end
